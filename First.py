01252a81e8bb177020b826e1ef53bd40561f604f
bokmacdev <fauck2002@gmail.com>
2026-02-24 17:00:16 +0800

    20260224-fugle

--- First.py
+++ First.py
@@ -1,1 +1,7 @@
-print("Let's GO")
+from fugle_marketdata import RestClient
+
+client = RestClient(api_key = 'NjhkN2JjOTUtZDU0NC00MmFmLTkzMTEtOThlM2I1MTBiN2FkIDAzMTZlZWI4LTQ2YWEtNGUyOS05NGY5LTY2ZmUxMzA4MmYzNQ==')
+stock = client.stock
+
+print(stock.historical.candles(**{"symbol": "2330", "from": "2026-02-23", "to": "2026-02-24", "fields": "open,high,low,close"}))
+
